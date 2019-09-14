4bcac5199cdfe9abaeb7196315434b668ac363f7
Miley Wright <dishaliekwright@yahoo.com>
2019-09-13 21:38:08 -0400

    completed header and Tabs section

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,17 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+function Tabs(items){
+    const tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.textContent = items.data.topics;
+    
+    document.querySelector('.topics').appendChild(tab);
+}
+
+axios.get(' https://lambda-times-backend.herokuapp.com/topics')
+.then(response => {
+    console.log(response)
+    Tabs(response)
+})
