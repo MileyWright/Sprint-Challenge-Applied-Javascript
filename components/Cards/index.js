2cbdb6ec5eeb3fd3dc1283e1053c1ff7adbb704b
Miley Wright <dishaliekwright@yahoo.com>
2019-09-13 23:44:15 -0400

    MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,47 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function cards(article) {
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const author =document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const image = document.createElement('img');
+    const authorName = document.createElement('span');
+
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(image);
+    author.appendChild(authorName);
+    
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container')
+    
+    headline.textContent = article.headline;
+    image.src = article.authorPhoto;
+    authorName.textContent = article.authorName;
+
+    return card
+}
+
+const cardContainer = document.querySelector('.cards-container')
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        console.log(response);
+        response.data.articles.bootstrap.forEach(article => {
+            cardContainer.appendChild(cards(article))
+        });
+        response.data.articles.javascript.forEach(article => {
+            cardContainer.appendChild(cards(article))
+        });
+        response.data.articles.jquery.forEach(article => {
+            cardContainer.appendChild(cards(article))
+        });
+        response.data.articles.technology.forEach(article => {
+            cardContainer.appendChild(cards(article))
+        });
+    })
